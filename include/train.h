4e256aa50dcf0bc06a123c0823048f60baf0f828
ulyanab1337-sys <ulyanab1337@gmail.com>
2026-05-13 08:32:11 +0300

    Update train.h

--- include/train.h
+++ include/train.h
@@ -1,23 +1,22 @@
-// Copyright 2026 NNTU-CS
+// Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TRAIN_H_
 #define INCLUDE_TRAIN_H_
 
 class Train {
  private:
-    struct Car {
-        bool light;
-        Car *next;
-        Car *prev;
-    };
-    int countOp;
-    Car *first;
+  struct Wagon {
+    bool bulb;
+    Wagon *nextWagon;
+    Wagon *prevWagon;
+  };
+  int totalMoves;
+  Wagon *entryPoint;
 
  public:
-    Train();
-    ~Train();
-    void addCar(bool light);
-    int getLength();
-    int getOpCount();
+  Train();
+  void appendWagon(bool bulbState);
+  int calcLength();
+  int getTotalMoves();
 };
 
 #endif  // INCLUDE_TRAIN_H_
